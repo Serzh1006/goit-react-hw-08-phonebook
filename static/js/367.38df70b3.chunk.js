483f6a162396aad47b2e682e56fe2de3113dbde6
Serzh1006 <skravchenko223@gmail.com>
2023-12-04 18:04:02 +0000

    Deploying to gh-pages from @ Serzh1006/goit-react-hw-08-phonebook@2e72f26832169d256261d3670dbfff2edf9179ed 🚀

---
+++ static/js/367.38df70b3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[367],{7367:function(e,t,i){i.r(t),i.d(t,{default:function(){return r}});var n="titleHome_titleContainer__KeTEf",l="titleHome_titleSection__sbho-",o="titleHome_title__X8ibr",c=i(3329),r=function(){return(0,c.jsx)("section",{className:l,children:(0,c.jsx)("div",{className:n,children:(0,c.jsx)("h1",{className:o,children:"PhoneBook"})})})}}}]);
+//# sourceMappingURL=367.38df70b3.chunk.js.map
